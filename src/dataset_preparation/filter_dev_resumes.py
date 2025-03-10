432f99052272261e1da889c95545bcdd03a931da
omkanekar28 <omkanekar28@gmail.com>
2025-03-10 22:52:22 +0530

    Reduced JD categories

--- src/dataset_preparation/filter_dev_resumes.py
+++ src/dataset_preparation/filter_dev_resumes.py
@@ -21,15 +21,8 @@ class ResumeClassifier:
             "Data Science": "Working with data, building machine learning models, deep learning, artificial intelligence, data visualization, statistics, and data analysis.",
             "Web Designing": "Creating website layouts, UI/UX design, working with HTML, CSS, JavaScript, Figma, Adobe XD, and responsive design principles.",
             "Java Developer": "Developing applications using Java, Spring Boot, Hibernate, Microservices, backend development, and software engineering principles.",
-            "SAP Developer": "Working with SAP modules, ABAP programming, SAP HANA, enterprise resource planning (ERP) solutions, and SAP integrations.",
-            "Automation Testing": "Developing test scripts, using Selenium, Cypress, Playwright, automated QA, test automation frameworks, and performance testing.",
-            "Python Developer": "Building applications using Python, Flask, Django, FastAPI, scripting, backend development, and data processing.",
             "DevOps Engineer": "Handling CI/CD pipelines, Docker, Kubernetes, cloud infrastructure (AWS, Azure, GCP), automation, and system reliability.",
-            "Network Security Engineer": "Implementing firewalls, network security protocols, ethical hacking, intrusion detection, cybersecurity, and VPN management.",
             "Database": "Working with SQL, MySQL, PostgreSQL, MongoDB, database administration, query optimization, and data modeling.",
-            "Hadoop": "Big data processing using Hadoop, Spark, HDFS, MapReduce, data pipelines, and distributed computing.",
-            "ETL Developer": "Extracting, transforming, and loading data, working with ETL tools like Informatica, Talend, DataStage, and SQL-based data pipelines.",
-            "Dotnet Developer": "Developing applications using .NET framework, C#, ASP.NET, MVC, Web API, and Windows applications.",
             "Testing": "Manual and automated testing, writing test cases, using tools like JIRA, Selenium, TestNG, performance testing, and software quality assurance."
         }
         print(f"Computing embeddings for pre-defined categories...")
@@ -78,8 +71,6 @@ class ResumeClassifier:
                     if category_score > highest_category_score:
                         highest_category_score = category_score
 
-                # TODO: Write logic to take resumes with respectable score in atleast one of the categories 
-                #       (Would be better to take some irrelevant ones too for generalisation of data).
                 if highest_category_score >= similarity_threshold:
                     self.output_dict['Filename'].append(file)
                     self.output_dict['Text'].append(text)
