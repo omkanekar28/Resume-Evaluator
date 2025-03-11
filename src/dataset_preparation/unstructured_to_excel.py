5858779f61b0ada25c11a9b9f2f8fb2f235f5d98
omkanekar28 <omkanekar28@gmail.com>
2025-03-11 20:05:50 +0530

    Started code for unstructured resume formatting

---
+++ src/dataset_preparation/unstructured_to_excel.py
@@ -0,0 +1,15 @@
+from preprocessing import Preprocessor
+
+
+class ResumeFormatter:
+    """
+    Class that handles operations related to formatting the given input resume files into 
+    structured excel format.
+    """
+
+    def __init__(self) -> None:
+        """
+        """
+        self.preprocessor = Preprocessor()
+    
+    # TODO: Write remaining code
