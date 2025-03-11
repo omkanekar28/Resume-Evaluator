d3f81fbd210f05092664aba517c7c470b202647b
omkanekar28 <omkanekar28@gmail.com>
2025-03-11 19:58:45 +0530

    Gathered PDFs, Images and Docx resumes

--- src/dataset_preparation/utils.py
+++
@@ -1,9 +0,0 @@
-def fancy_print(text: str, padding_spaces: int = 6) -> None:
-    """
-    Prints the given text in a decorative box.
-    """
-    border_length = len(text) + padding_spaces  # Adjust for padding and borders
-    print('*' * border_length)
-    surrounding_empty_spaces = int((padding_spaces - 2) / 2)
-    print(f"*{' ' * surrounding_empty_spaces}{text}{' ' * surrounding_empty_spaces}*")
-    print('*' * border_length)
